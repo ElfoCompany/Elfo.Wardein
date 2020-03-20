d0d0e6d2231cebfccf7b9390f129fbbe2128f083
Mirko Bellabarba <mirko.bellabarba@elfo.net>
2020-03-20 12:44:33 +0100

    wip #53
    * added implementation for OracleWatcherPersistenceService
    * adeed method in oracleHelper to execute procedures

--- Elfo.Wardein.Core/Persistence/OracleWatcherPersistenceService.cs
+++ Elfo.Wardein.Core/Persistence/OracleWatcherPersistenceService.cs
@@ -1,7 +1,9 @@
 ﻿using Elfo.Firmenich.Wardein.Abstractions.Watchers;
 using Elfo.Wardein.Core.Helpers;
+using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,15 +12,37 @@ namespace Elfo.Firmenich.Wardein.Core.Persistence
     public class OracleWatcherPersistenceService : IAmWatcherPersistenceService
     {
         private readonly OracleConnectionConfiguration oracleConnectionConfiguration;
+        private readonly OracleHelper oracleHelper;
 
         public OracleWatcherPersistenceService(OracleConnectionConfiguration oracleConnectionConfiguration)
         {
             this.oracleConnectionConfiguration = oracleConnectionConfiguration;
+            this.oracleHelper = new OracleHelper(oracleConnectionConfiguration);
         }
 
-        public Task<WatcherStatusResult> UpsertCurrentStatus(int watcherConfigurationId, int applicationId, string applicationHostname, bool isHealthy, Exception failureException = null)
+        public async Task<WatcherStatusResult> UpsertCurrentStatus(int watcherConfigurationId, int applicationId, string applicationHostname, bool isHealthy, Exception failureException = null)
         {
-            throw new NotImplementedException();
+            return await this.oracleHelper.CallProcedureAsync<WatcherStatusResult>(
+                packageName: "PKG_WRD",
+                procedureName: "PRC_UPS_WTCH_RES",
+                howToGetResult: (parameters) =>
+                {
+                    int.TryParse(parameters.FirstOrDefault(x => x.ParameterName == "po_flr_count")?.Value?.ToString(), out int errorCount);
+                    bool wasHealthy = parameters.FirstOrDefault(x => x.ParameterName == "po_prv_status")?.Value?.ToString()?.ToUpperInvariant() == "Y";
+                    return new WatcherStatusResult()
+                    {
+                        FailureCount = errorCount,
+                        PreviousStatus = wasHealthy
+                    };
+                },
+                new OracleParameter("p_wtchr_cnfg_id", OracleDbType.Int32, watcherConfigurationId, System.Data.ParameterDirection.Input),
+                new OracleParameter("p_appl_id", OracleDbType.Int32, applicationId, System.Data.ParameterDirection.Input),
+                new OracleParameter("p_hostname", OracleDbType.Varchar2, applicationId, System.Data.ParameterDirection.Input),
+                new OracleParameter("p_is_healthy", OracleDbType.Varchar2, applicationId, System.Data.ParameterDirection.Input),
+                new OracleParameter("p_flr_msg", OracleDbType.Varchar2, applicationId, System.Data.ParameterDirection.Input),
+                new OracleParameter("po_flr_count", OracleDbType.Int32, applicationId, System.Data.ParameterDirection.Output),
+                new OracleParameter("po_prv_status", OracleDbType.Varchar2, applicationId, System.Data.ParameterDirection.Output)
+            );
         }
     }
 }
